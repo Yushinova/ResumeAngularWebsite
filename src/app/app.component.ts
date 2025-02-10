14613e63645106349e8e245cf7817dfd38af21b6
Yushi_np28@top-academy.ru <Yushi_np28@top-academy.ru>
2025-02-10 15:45:14 +0300

    add pic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ResumeAngularWebsite';
+  name: string = "Yushinova Tatyana"
+ about: string = "I'm 40, but I'm still learning and developing! Graduated from the STEP Academy. I have experience in team project development."
+ technologies: string[] = ["ADO.NET", "SQL", "Network Programming", "ASP.NET", "Angular", "React"]
+ languages: string[] = ["C++", "C#", "HTML", "CSS", "JavaScript", "TypeScript"]
+ rangeTechnologies: number[] = this.setRange(this.technologies)
+ rangeLanguages: number[]=this.setRange(this.languages)
+ isExperience: boolean = true
+ setRange(mas: string[]): number[]{
+  let temp:number[]= new Array;
+  for(let i = 0; i < mas.length/3; i++){
+     temp.push(i);
+  }
+  return temp;
+}
+
 }
